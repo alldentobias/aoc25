05714d3b2a65a968ef0d92a65ee57ef99729f98b
alldentobias <alldentobias@gmail.com>
2025-12-01 09:02:32 +0100

    Operationalize it a bit

--- day1/day1.py
+++ day1/day1.py
@@ -1,12 +1,24 @@
 def main():
-    part1()
-    part2()
+    expected_example_result_part1 = 3
+    expected_example_result_part2 = 6
+    
+    print("Running example input for part 1...")
+    assert(part1('example.txt') == expected_example_result_part1)
+    print("Example input for part 1 passed!")
+    print("Running actual input for part 1...")
+    print(f"Part 1 result: {part1('input.txt')}")
 
-def part1():
+    print("Running example input for part 2...")
+    assert(part2('example.txt') == expected_example_result_part2)
+    print("Example input for part 2 passed!")
+    print("Running actual input for part 2...")
+    print(f"Part 2 result: {part2('input.txt')}")
+
+def part1(file):
     zeroes = 0
     pos = 50
 
-    with open('input.txt', 'r') as f:
+    with open(file, 'r') as f:
         for line in f:
             dir = 1 if line[0].lower() == 'r' else -1
             count = int(line[1:])
@@ -14,13 +26,13 @@ def part1():
 
             if pos == 0: 
                 zeroes += 1
-    print(f"Ended on zeroes: {zeroes}")
+    return zeroes
 
-def part2():
+def part2(file):
     zeroes = 0
     pos = 50
     lock = range(0,100)
-    with open('input.txt', 'r') as f:
+    with open(file, 'r') as f:
         for line in f:
             dir = 1 if line[0].lower() == 'r' else -1
             count = int(line[1:])
@@ -35,12 +47,7 @@ def part2():
                 if pos == 0:
                     zeroes += 1
 
-    print(f"Traversed zeroes: {zeroes}")
-
-
-
-            
-
+    return zeroes
 
 if __name__ == "__main__":
     main()
